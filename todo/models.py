52cff89b59ff9a4822a6833f513caf772e198469
Dulany W <githubemail@nym.hush.com>
2023-01-27 23:09:07 -0500

    Expanded Todo Model Definition

--- todo/models.py
+++ todo/models.py
@@ -11,11 +11,14 @@ class Topic(models.Model):
 
 class TodoItem(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    description = models.TextField()
+    title = models.CharField(max_length=200, default="New Todo Item")
+    notes = models.TextField()
+    next_step_owner = models.CharField(max_length=200, null=True, blank=True)
     completed = models.BooleanField(default=False)
     due_date = models.DateTimeField(blank=True, null=True)
+    next_checkin = models.DateTimeField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.description
+        return self.title
--- todo/views.py
+++ todo/views.py
@@ -6,3 +6,27 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the todo app index.")
     #note to self - ^HttpResponse is not equivalent to HTTPResponse!
+
+# ***Topics views***
+# create a new topic
+def topic_create(request):
+    pass
+
+# update a topic
+def topic_update(request, topic_id):
+    pass
+
+
+# ***TodoItem views***
+# view details of a single todo item
+def todo_detail(request, todo_id):
+    pass
+
+# create a new todo item
+def todo_create(request):
+    pass
+
+# update a todo item
+def todo_update(request, todo_id):
+    pass
+
